1f73fac672eabab8bfe0fc5d27a7c768f1da4e47
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-06-08 02:32:21 +0900

    jinja2 base temp applied

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print('push!')
     return render_template('index.html')
+
+@app.route('/Login')
+def login():
+    return render_template('Login.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
