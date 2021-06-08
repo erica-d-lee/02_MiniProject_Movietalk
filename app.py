a3cca2a367a8cd73691074995f9198cf9c8f9d07
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-06-08 17:12:41 +0900

    aa

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
+client = MongoClient('localhost', 207017)
+db = client.week2
+
+
+db.users.insert_one
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/Login')
 def login():
@@ -17,6 +25,14 @@ def login():
 def search():
     return render_template('search.html')
 
-if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
 
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run('0.0.0.0', port=5000, debug=True)
