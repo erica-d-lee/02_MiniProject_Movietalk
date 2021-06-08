7c9fccb495475087da21d916e46dbbe8bad2b3b1
leedmeen <leedmeen@gmail.com>
2021-06-08 16:03:11 +0900

    메인페이지, index.html css 변경

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-
 app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbmovietalk
 
 @app.route('/')
 def home():
@@ -12,10 +15,14 @@ def home():
 def login():
     return render_template('Login.html')
 
-
 @app.route('/search/')
 def search():
     return render_template('search.html')
+
+@app.route('/main/getlist', methods=['GET'])
+def get_list():
+    movies = list(db.movie.find({}, {'_id': False}))
+    return jsonify({'all_movies': movies})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
