3ac68deb82d6654df5200f6f635ae40f2f1c5c4c
SooYeonKim <lovesu5632@gmail.com>
2021-06-08 23:01:25 +0900

    회원가입, 로그인

--- app.py
+++ app.py
@@ -1,39 +1,68 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
-
-<<<<<<< HEAD
-client = MongoClient('localhost', 207017)
-db = client.week2
-
-
-db.users.insert_one
-
+import datetime
+import hashlib
+import jwt
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-=======
-app = Flask(__name__)
+SECRET_KEY = 'MOVIETALK'
 
-from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbmovietalk
->>>>>>> 7c9fccb495475087da21d916e46dbbe8bad2b3b1
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-@app.route('/Login')
+@app.route('/login')
 def login():
-    return render_template('Login.html')
+    return render_template('login.html')
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    nickname_receive = request.form['nickname_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+        "nickname": nickname_receive
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 @app.route('/search/')
 def search():
     return render_template('search.html')
 
-<<<<<<< HEAD
-=======
 @app.route('/main/getlist', methods=['GET'])
 def get_list():
     movies = list(db.movie.find({}, {'_id': False}))
@@ -41,15 +70,3 @@ def get_list():
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
->>>>>>> 7c9fccb495475087da21d916e46dbbe8bad2b3b1
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run('0.0.0.0', port=5000, debug=True)
